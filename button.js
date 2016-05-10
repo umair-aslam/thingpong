5fc3d52643e1840c70c54e295578adf8891b422e
Umair Aslam <uaslam@coldlight.com>
2016-05-10 15:13:07 -0400

    added a second button

--- button.js
+++ button.js
@@ -1,14 +1,28 @@
 var wpi = require('wiring-pi');
-
 wpi.setup('wpi');
-wpi.pinMode(7, wpi.INPUT);
-wpi.pullUpDnControl(7, wpi.PUD_UP);
-wpi.wiringPiISR(7, wpi.INT_EDGE_FALLING, function(){
-	handleButton(7);
+
+var teamAplus = 15;
+var teamBplus = 25
+
+wpi.pinMode(teamAplus, wpi.INPUT);
+wpi.pullUpDnControl(teamAplus, wpi.PUD_UP);
+wpi.wiringPiISR(teamAplus, wpi.INT_EDGE_FALLING, function(){
+	handleButton(greenButton);
 });
+
+wpi.pinMode(teamBplus, wpi.INPUT);
+wpi.pullUpDnControl(teamBplus, wpi.PUD_UP);
+wpi.wiringPiISR(teamBplus, wpi.INT_EDGE_FALLING, function(){
+	handleButton(teamBplus);
+});
+
 
 function handleButton(pin) {
 	if(wpi.digitalRead(pin)) {
-		console.log('Pressed ' + pin)
+		if(pin === teamAplus) {
+			console.log('team A ++' + pin)
+		} else if (pin === teamBplus) {
+			console.log('team B ++ ' + pin)
+		}
 	}
 }
