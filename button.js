d373e0b2e9f26c9ceba1a7a0488c242362e0a3bb
Umair Aslam <uaslam@coldlight.com>
2016-05-10 14:34:57 -0400

    changed buttons working

--- button.js
+++ button.js
@@ -3,6 +3,12 @@ var wpi = require('wiring-pi');
 wpi.setup('wpi');
 wpi.pinMode(7, wpi.INPUT);
 wpi.pullUpDnControl(7, wpi.PUD_UP);
-wpi.wiringPiISR(7, wpi.INT_EDGE_FALLING, function(delta){
-	console.log('Pin 7 changed to Low (', delta, ')');
+wpi.wiringPiISR(7, wpi.INT_EDGE_FALLING, function(){
+	handleButton(7);
 });
+
+function handleButton(pin) {
+	if(wpi.digitalRead(pin)) {
+		console.log('Pressed ' + pin)
+	}
+}
