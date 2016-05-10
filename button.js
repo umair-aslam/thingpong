496720d0bc8f7b22741dde0fb4a479c5eb6d5259
Umair Aslam <uaslam@coldlight.com>
2016-05-10 16:54:42 -0400

    all four buttons

--- button.js
+++ button.js
@@ -7,45 +7,33 @@ var started = false;
 var clock = null;
 
 var teamAplus = 15;
-var teamAminus = 5;
+var teamAminus = 6;
 
 var teamBplus = 25;
-var teamBminus = 23;
+var teamBminus = 14;
 
 wpi.pinMode(teamAplus, wpi.INPUT);
 wpi.pullUpDnControl(teamAplus, wpi.PUD_UP);
 wpi.wiringPiISR(teamAplus, wpi.INT_EDGE_FALLING, function() {
-	handleButton(teamAplus);
+	setTimeout(score(pin), 250);
 });
 wpi.pinMode(teamAminus, wpi.INPUT);
 wpi.pullUpDnControl(teamAminus, wpi.PUD_UP);
 wpi.wiringPiISR(teamAminus, wpi.INT_EDGE_FALLING, function() {
-	handleButton(teamAminus);
+	setTimeout(score(pin), 250);
 });
 
 wpi.pinMode(teamBplus, wpi.INPUT);
 wpi.pullUpDnControl(teamBplus, wpi.PUD_UP);
 wpi.wiringPiISR(teamBplus, wpi.INT_EDGE_FALLING, function() {
-	handleButton(teamBplus);
+	setTimeout(score(pin), 250);
 });
 wpi.pinMode(teamBminus, wpi.INPUT);
 wpi.pullUpDnControl(teamBminus, wpi.PUD_UP);
 wpi.wiringPiISR(teamBminus, wpi.INT_EDGE_FALLING, function() {
-	handleButton(teamBminus);
+	setTimeout(score(pin), 250);
 });
 
-
-function handleButton(pin) {
-	if (wpi.digitalRead(pin)) {
-		if (false === started) {
-			started = true;
-			clock = setTimeout(score(pin), 250);
-		} else {
-			started = false;
-			clearTimeout(clock);
-		}
-	}
-}
 
 function score(pin) {
 	if (pin === teamAplus) {
